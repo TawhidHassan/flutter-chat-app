92d634bc82219fa0f8cef0ffd7a4101ced084a5f
sifat007 <sifat27.ht@gmail.com>
2021-02-22 11:37:31 +0600

    signup by firebase done

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/services/auth.dart';
 import 'package:flutter_chat_app/widgets/widget.dart';
 
+import 'chatsRooms.dart';
 import 'forgetpassword.dart';
 
 class SignUp extends StatefulWidget {
@@ -12,11 +14,34 @@ class _SignUpState extends State<SignUp> {
   TextEditingController emailEditingController = new TextEditingController();
   TextEditingController passwordEditingController = new TextEditingController();
   TextEditingController usernameEditingController = new TextEditingController();
-  @override
+
+  final formKey = GlobalKey<FormState>();
+  bool isLoading = false;
+
+  AuthService authService = new AuthService();
+
+  singUp() async {
+    if(formKey.currentState.validate()){
+      setState(() {
+        isLoading = true;
+      });
+
+      authService.signUpWithEmailAndPassword(emailEditingController.text, passwordEditingController.text)
+      .then((result){
+        if(result != null){
+          Navigator.pushReplacement(context, MaterialPageRoute(
+              builder: (context) => ChatRoom()
+          ));
+        }
+      });
+    }
+  }
+
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: appBarMain(context),
-        body: SingleChildScrollView(
+        body: isLoading ? Container(child: Center(child: CircularProgressIndicator(),),) : SingleChildScrollView(
           child: Container(
             height: MediaQuery.of(context).size.height-50,
             alignment: Alignment.bottomCenter,
@@ -25,26 +50,43 @@ class _SignUpState extends State<SignUp> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  TextField(
-                    controller: usernameEditingController,
-                      decoration: textFieldInputDecoration("User Name") ,
-                      style: simpleTextStyle()
-                  ),TextField(
-                    controller: emailEditingController,
-                      decoration: textFieldInputDecoration("Email") ,
-                      style: simpleTextStyle()
-                  ),TextField(
-                    controller: passwordEditingController,
-                      decoration: textFieldInputDecoration("Password") ,
-                      style: simpleTextStyle()
-                  ),
+                 Form(
+                   key: formKey,
+                   child: Column(
+                     children: [
+                       TextFormField(
+                           validator: (val){
+                             return val.isEmpty || val.length < 3 ? "Enter Username 3+ characters" : null;
+                           },
+                           controller: usernameEditingController,
+                           decoration: textFieldInputDecoration("User Name") ,
+                           style: simpleTextStyle()
+                       ),TextFormField(
+                           validator: (val){
+                             return RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ?
+                             null : "Enter correct email";
+                           },
+                           controller: emailEditingController,
+                           decoration: textFieldInputDecoration("Email") ,
+                           style: simpleTextStyle()
+                       ),TextFormField(
+                           validator:  (val){
+                             return val.length < 6 ? "Enter Password 6+ characters" : null;
+                           },
+                           controller: passwordEditingController,
+                           decoration: textFieldInputDecoration("Password") ,
+                           style: simpleTextStyle()
+                       ),
+                     ],
+                   ),
+                 ),
 
                   SizedBox(
                     height: 16,
                   ),
                   GestureDetector(
                     onTap: () {
-                      // signIn();
+                      singUp();
                     },
                     child: Container(
                       padding: EdgeInsets.symmetric(vertical: 16),
